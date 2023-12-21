4ae263c56a11a2bf9b5ac86e2fb526b121e78463
Moein Arabi <moein.mo81@gmail.com>
2023-12-21 13:16:30 +0330

    Add user status attribute

--- homework1/problem1/utils/user.py
+++ homework1/problem1/utils/user.py
@@ -1,7 +1,14 @@
+from enum import IntEnum
 import socket
+
+
+class UserStatus(IntEnum):
+    AVAILABLE: int
+    BUSY: int
 
 
 class User:
     def __init__(self, username: str, socket: socket.socket) -> None:
         self.username = username
         self.socket = socket
+        self.status = UserStatus.AVAILABLE
