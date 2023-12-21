926d5f59d4a99f4f1b5ff28f07b37eb97c4cc090
Moein Arabi <moein.mo81@gmail.com>
2023-12-21 12:25:56 +0330

    Add send time attribute to Message

--- homework1/problem1/utils/message.py
+++ homework1/problem1/utils/message.py
@@ -1,15 +1,16 @@
 class Message:
-    def __init__(self, type: str, sender: str, receiver: str, content: str) -> None:
+    def __init__(self, type: str, sender: str, receiver: str, content: str, send_time: str) -> None:
         self.type = type
         self.sender = sender
         self.receiver = receiver
         self.content = content
+        self.send_time = send_time
 
     def marshal(self) -> bytes:
         """Marshal message into bytes
-        Format: sender;receiver;content
+        Format: type;sender;receiver;content;send_time
         """
-        return f'{self.type};{self.sender};{self.receiver};{self.content}'.encode()
+        return f'{self.type};{self.sender};{self.receiver};{self.content};{self.send_time}'.encode()
 
     @classmethod
     def unmarshal(cls, data: bytes):
