c8cf3da7c37fa8f0aa05bc705dc61b1a0b5160e9
Moein Arabi <moein.mo81@gmail.com>
2023-12-22 09:34:12 +0330

    Add password field

--- homework1/problem1/utils/user.py
+++ homework1/problem1/utils/user.py
@@ -1,5 +1,6 @@
 from enum import IntEnum
 import socket
+import hashlib
 
 
 class UserStatus(IntEnum):
@@ -10,5 +11,12 @@ class UserStatus(IntEnum):
 class User:
     def __init__(self, username: str, socket: socket.socket) -> None:
         self.username = username
+        self.password = None
         self.socket = socket
         self.status = UserStatus.AVAILABLE
+
+    def set_password(self, password: str, salt: str):
+        self.password = hashlib.sha256((password + salt).encode()).hexdigest()
+
+    def check_password(self, password: str, salt: str) -> bool:
+        return self.password == hashlib.sha256((password + salt).encode()).hexdigest()
